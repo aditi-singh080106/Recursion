a6a1e2b785283ff36c4852d55dcd688a7e6c445b
Aditi Singh <aditi08092003@gmail.com>
2025-10-03 16:18:08 +0530

    Create Print1toN.java

---
+++ Print1toN.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void print(int n){
+        if(n==0) return;
+        print(n-1);
+        System.out.print(n+"\t");
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter a number : ");
+	    int num = sc.nextInt();
+	    print(num);
+	}
+}
