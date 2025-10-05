b7c95eee39683b5a2b1127bf768978a415efad3a
Aditi Singh <aditi08092003@gmail.com>
2025-10-05 12:26:55 +0530

    Update MazePath.java

--- MazePath.java
+++ MazePath.java
@@ -7,13 +7,20 @@ public class Main
         int downways=maze(row+1,col,rsize,csize);
         return rightways+downways;
     }
+    public static int maze2(int n,int m){
+        if(n==1||m==1) return 1;
+        int rightways=maze2(n,m-1);
+        int downways=maze2(n-1,m);
+        return rightways+downways;
+    }
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
 	    System.out.print("Enter rows : ");
 	    int n=sc.nextInt();
 	    System.out.print("Enter columns : ");
 	    int m=sc.nextInt();
-	    System.out.print("Total number of ways : "+maze(1,1,n,m));
+	    System.out.println("Total number of ways : "+maze(1,1,n,m));
+	    System.out.print("Total number of ways : "+maze2(n,m));
 	    
 	}
 }
