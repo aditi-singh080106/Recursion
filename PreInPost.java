44751e68864300f90548accc4302108ef889b5a8
Aditi Singh <aditi08092003@gmail.com>
2025-10-05 14:19:30 +0530

    Update PreInPost.java

--- PreInPost.java
+++ PreInPost.java
@@ -3,11 +3,11 @@ public class Main
 {
     public static void pip(int n){
         if(n==0) return ;
-        System.out.print(n+" "); // pre
+        System.out.print("pre: "+n+" "); // pre
         pip(n-1);
-        System.out.print(n+" "); // in
+        System.out.print("in: "+n+" "); // in
         pip(n-1);
-        System.out.print(n+" "); // post
+        System.out.print("post: "+n+" "); // post
     }
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
